e37d6a926edc85c276f4a1d821ab07a982c5ee04
s.a.sadaro <s.a.sadaro@gmail.com>
2017-05-20 11:58:06 -0400

    mobile changes

--- main js/main.js
+++ main js/main.js
@@ -51,5 +51,17 @@ $('a[href*=#]').on('click', function(event){
    event.preventDefault();
    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
 });
-	});
+	
+	
+	});	
+
+
+
+
+
+
+	
+	
+	
+	
 	 	
